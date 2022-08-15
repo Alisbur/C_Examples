fb1e5ec1ca0121059e962513b9bf9b25907a95f7
Eugen <mail-4-eugen@yandex.ru>
2022-08-15 23:15:08 +0300

    После 3-й лекции

---
+++ Example201 - Zamena_bukv/Program.cs
@@ -0,0 +1,32 @@
+﻿// Console.Clear();
+// Console.WriteLine("Введите текст");
+// string text = Console.ReadLine();
+// string result = String.Empty;
+
+// for(int i=0;i<text.Length;i++)
+//     switch(text[i])
+//     {
+//         case ' ': {result = result + '-';break;}
+//         case 'к': {result = result + 'К';break;}
+//         case 'С': {result = result + 'с';break;}
+//         default: {result = result + text[i];break;}
+//     }
+
+// Console.WriteLine(result);
+
+string ReplaceSym(string text, char c1, char c2){
+    string result = String.Empty;
+
+    for(int i=0;i<text.Length;i++){
+        if (text[i] == c1) result=result+c2;
+        else result= result+text[i];        
+    }
+    return(result);
+}
+string someText = "кывдалткк ва фы ССС сс ККК ккк ывтывадлш узщшкцчь";
+
+someText = ReplaceSym(someText,' ','|');
+someText = ReplaceSym(someText,'С','с');
+someText = ReplaceSym(someText,'к','К');
+
+Console.WriteLine(someText);
---
+++ Example202 - Sort_Mas/Program.cs
@@ -0,0 +1,50 @@
+﻿int[] SortMas(int[] mas, int type){
+    int temp;
+    int maxInd;
+  
+    for(int i=0;i<mas.Length-1;i++){
+        maxInd=i;
+        for(int j=i+1;j<mas.Length;j++){
+            if((type!=0)&&(mas[j]>mas[maxInd]))
+                maxInd=j;
+            if((type==0)&&(mas[j]<mas[maxInd]))
+                maxInd=j;
+            }   
+        temp=mas[i];
+        mas[i]=mas[maxInd];
+        mas[maxInd]=temp;
+    }
+    return mas;
+}
+
+int[] FillMas(int len){
+    int[] mas = new int[len];
+    
+    for(int i=0;i<mas.Length;i++)
+        mas[i]=new Random().Next(1,20);
+    
+    return(mas);
+}
+
+void PrintMas(int[] mas){
+    for(int i=0;i<mas.Length;i++)
+        Console.Write($"{mas[i]}, ");
+    Console.WriteLine();
+}
+
+Console.Clear();
+Console.WriteLine("Введите длину массива");
+int len = int.Parse(Console.ReadLine());
+
+int[] mas = new int[len];
+
+Console.WriteLine("Массив: ");
+mas=FillMas(len);
+PrintMas(mas);
+
+Console.WriteLine("Введите тип сортировки (0 - по возр., 1 - по убыв.)");
+int type = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Отсортированный массив: ");
+mas=SortMas(mas,type);
+PrintMas(mas);
